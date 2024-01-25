cd7b3e5d821f271a5721582ecbcbbd77377c7d16
sailaptopservicehosur <154626162+sailaptopservicehosur@users.noreply.github.com>
2024-01-25 12:33:32 +0530

    Add files via upload

--- Question 5/index.js
+++ Question 5/index.js
@@ -1,1 +1,19 @@
-
+function removeDuplicates(arr) {
+    const uniqueSet = new Set();
+    const result = [];
+  
+    for (const item of arr) {
+      if (!uniqueSet.has(item)) {
+        uniqueSet.add(item);
+        result.push(item);
+      }
+    }
+  
+    return result;
+  }
+  
+  const inputArray = [1, 2, 3, 4, 5, 1, 6, 2, 7, 1, 3];
+  const outputArray = removeDuplicates(inputArray);
+  
+  console.log(outputArray); // Output: [1, 2, 3, 4, 5, 6, 7]
+  
